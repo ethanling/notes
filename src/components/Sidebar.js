d3fa696da4b120b1f6e189376f890864c185e5c3
Ethan Ling <ethanling1@gmail.com>
2019-11-27 15:19:08 +0000

    Sidebar animations

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Sidebar = (props) => {
 
 	const StyledSidebar = styled.aside`
 		background-color: #1d1d1d;
 		grid-column: 2;
 		grid-row: 2;
-		${props.toggle ? ('height: 0') : ('height: 100vh;')}
+		${props => props.toggle ? ('height: 0') : ('height: 100vh;')}
 		transition: all .3s ease;
 		z-indedx. 1;
 	`;
+
+
+const Sidebar = (props) => {
+
 	
 	return (
 		<StyledSidebar>
